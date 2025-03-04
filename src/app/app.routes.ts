fae1d510be773f249ed95e287ebb6a3ac42ddf79
karim hatem <khatem@4sw.app>
2025-03-05 00:45:52 +0200

    adding routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './modules/user-list/user-list.component';
+import { UserRepoComponent } from './modules/user-repo/user-repo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'users', pathMatch: 'full' },
+    { path: 'users', component: UserListComponent },
+    { path: 'users/:name', component: UserRepoComponent },
+];
